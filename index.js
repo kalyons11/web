edd6a6d0584bee5e81307f601bb4a2b0e68a1522
kalyons11 <kevinandrewlyons@gmail.com>
2016-12-24 12:07:08 -0500

    Fixing issue with uuid module.

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ var fs = require("fs");
 var utils = require('./utils/utils');
 var moment = require('moment');
 var https = require("https");
-const uuidV4 = require('uuid/v4');
 var JSON = require('./utils/JSON').JSON;
 
 var app = express();
@@ -12,48 +11,6 @@ var app = express();
 var winston = require('winston');
 
 require('winston-loggly');
-
-app.all(config.do, function(req, res, next) {
-    var postDataRaw = {
-        'schema_url': 'https://localytics-files.s3.amazonaws.com/schemas/eventsApi/v0.json',
-        'app_uuid': utils.decrypt(config.appId),
-        'customer_id': 'Random User',
-        'event_name': 'Page_Visit',
-        'event_time': (new Date()).getTime(),
-        'uuid': uuidV4(),
-        'attributes' : {
-            'url': req.url
-        }
-    };
-    var postData = JSON.stringify(postDataRaw);
-    var auth = utils.decrypt(config.apiKey) + ':' + utils.decrypt(config.secret);
-    var options = {
-        host: config.host,
-        port: config.port,
-        path: config.path,
-        method: 'POST',
-        headers: {
-            'Accept': '*/*',
-            'Authorization': 'Basic ' + new Buffer(auth).toString('base64'),
-            'Content-Type': 'application/json',
-            'Content-Length': postData.length
-        }
-    };
-    https.request(options, function(response) {
-        var body = "";
-        response.on('data', function(data) {
-            body += data.toString('utf-8');
-        });
-        response.on('end', function() {
-            //here we have the full response, html or json object
-            //console.log(postData);
-            next();
-        })
-        response.on('error', function(e) {
-            console.log("Got error: " + e.message);
-        });
-    }).write(postData);
-});
 
 app.use(express.static('public'));
 
